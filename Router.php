3cdb300da51685186e1e82ce2f3b6f9bc223e347
Piero Dev <pierodev21@gmail.com>
2025-01-21 22:14:07 -0500

    registrando url y sus metodos

--- Router.php
+++ Router.php
@@ -1,8 +1,30 @@
 <?php
 namespace MVC;
 class Router {
+   public $rutasGET = [];
+   public $rutasPOST = [];
  public function __construct()
  {
     echo "Creando el router";
+ }
+
+ public function get($url, $fn){
+    $this->rutasGET[$url] = $fn;
+ }
+
+ public function comprobarRutas(){
+   $urlActual = $_SERVER['PATH_INFO'] ?? '/';
+   $metodo = $_SERVER['REQUEST_METHOD'];
+
+   if($metodo === 'GET'){
+      $fn = $this->rutasGET[$urlActual] ?? null;
+   }
+
+   if($fn){
+      call_user_func($fn,$this);
+
+   } else {
+      echo "Pagina no encontrada";
+   }
  }
 }
--- public/index.php
+++ public/index.php
@@ -3,4 +3,12 @@
 use MVC\Router;
 
 require_once __DIR__ . '/../includes/app.php';
+
+
 $router = new Router();
+$router->get("/nosotros","function_nosotros");
+$router->get("/contacto","function_contacto");
+$router->get("/admin","function_admin");
+
+
+$router->comprobarRutas();
